8c3b4a0e1186f3a73a8bcc1b1d855271d8e2bf48
devsandrobatista <sandro887santos@gmail.com>
2026-09-12 19:17:43 -0300

    Projeto ainda não terminado
    Falta colocar o menu para funcionar na versão mobile.

---
+++ projetos/projeto10/assets/js/script.js
@@ -0,0 +1,40 @@
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-header");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("header__link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+/////////////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero");
